d75d63360d1613d3223e319669e033f8753f947e
Vova Abdrakhmanov <369565@gmail.com>
2016-03-17 18:01:11 +0300

    [add] minimalistic parser and convert to given type

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,41 @@
+mod parser;
+
 use std::path::Path;
 use std::collections::HashMap;
-use std::io::{self, BufReader};
-use std::io::prelude::*;
+use std::io::{BufReader, Read};
 use std::fs::File;
+use std::str::FromStr;
 
-type IniData = HashMap<String, HashMap<String, String>>;
+use parser::{parse_line, Parsed};
+
+type IniParsed = HashMap<String, HashMap<String, String>>;
 
 #[derive(Debug)]
-pub struct Ini(IniData);
+pub struct Ini(IniParsed);
 
 impl<'a> Ini {
     fn new() -> Ini {
         Ini(HashMap::new())
     }
-    pub fn from_file<S: AsRef<Path> + ?Sized>(path: &S) -> Ini {
-        let file = File::open(path)
-                        .ok()
-                        .expect(&format!("Can't open `{}` file!", path.as_ref().display()));
-        let reader = BufReader::new(file);
+    fn from_string(string: &str) -> Ini {
         let mut result = Ini::new();
         let mut section_name = String::new();
         let mut entry_list = HashMap::new();
-        for line in reader.lines().filter_map(|l| l.ok()) {
-            println!("line = `{}`", line);
-            if line.contains('[') && line.contains(']') {
-                let left_pos = line.find('[').unwrap() + 1;
-                let right_pos = line.find(']').unwrap();
-                if section_name.len() != 0 {
-                    result.0.insert(section_name, entry_list.clone());
-                    entry_list.clear();
+        for (i, line) in string.lines().enumerate() {
+            match parse_line(&line) {
+                Parsed::Section(name) => {
+                    if section_name.len() != 0 {
+                        result.0.insert(section_name, entry_list.clone());
+                        entry_list.clear();
+                    }
+                    section_name = name;
                 }
-                section_name = (&line[left_pos..right_pos]).to_owned();
-            } else if !line.starts_with(';') {
-                let vec: Vec<&str> = line.split('=').collect();
-                if vec.len() < 2 { continue; }
-                let token = vec[0].trim_right();
-                let value = if vec[1].contains(';') {
-                    vec[1].split(';').nth(0).unwrap().trim()
-                } else {
-                    vec[1].trim()
-                };
-                entry_list.insert(token.to_owned(), value.to_owned());
-            }
+                Parsed::Value(name, value) => {
+                    entry_list.insert(name, value);
+                }
+                Parsed::Error(msg) => println!("line {}: error: {}", i, msg),
+                _ => (),
+            };
         }
         // add last section
         if section_name.len() != 0 {
@@ -50,11 +44,65 @@ impl<'a> Ini {
         }
         result
     }
-    pub fn from_buffer<S: Into<String>>(buf: S) -> Ini {
-        unimplemented!()
+
+    pub fn from_file<S: AsRef<Path> + ?Sized>(path: &S) -> Ini {
+        let file = File::open(path)
+                       .ok()
+                       .expect(&format!("Can't open `{}`!", path.as_ref().display()));
+        let mut reader = BufReader::new(file);
+        let mut buffer = String::new();
+        let _ = reader.read_to_string(&mut buffer)
+                      .ok()
+                      .expect(&format!("Can't read `{}`!", path.as_ref().display()));
+        Ini::from_string(&buffer)
     }
-    pub fn section<S: Into<String>>(&'a self, name: S) -> Option<&'a HashMap<String, String>> {
-        let name = name.into();
-        self.0.get(&name)
+    pub fn from_buffer<S: Into<String>>(buf: S) -> Ini {
+        Ini::from_string(&buf.into())
+    }
+    fn get_raw(&'a self, section: &str, key: &str, default: &str) -> String {
+        let s = self.0.get(section);
+        let result = match s {
+            Some(hm) => {
+                let k = hm.get(key);
+                let value = match k {
+                    Some(v) => v.to_owned(),
+                    None => default.to_owned(),
+                };
+                value
+            }
+            None => default.to_owned(),
+        };
+        result
+    }
+    pub fn get<T: FromStr>(&'a self, section: &str, key: &str, default: &str) -> T {
+        self.get_raw(section, key, default)
+            .parse()
+            .ok()
+            .expect(&format!("cannot convert to given type"))
+    }
+    pub fn get_vec<T: FromStr>(&'a self, section: &str, key: &str, default: &str) -> Vec<T> {
+        self.get_raw(section, key, default)
+            .split(',')
+            .map(|s| s.trim().parse().ok().expect(&format!("cannot convert {} to given type", s)))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_float() {
+        let ini = Ini::from_string("[section]\nname=10.5");
+        let name: f64 = ini.get("section", "name", "0");
+        assert_eq!(name, 10.5);
+    }
+
+    #[test]
+    fn test_float_vec() {
+        let ini = Ini::from_string("[section]\nname=1.2, 3.4, 5.6");
+        let name: Vec<f64> = ini.get_vec("section", "name", "0");
+        assert_eq!(name, [1.2, 3.4, 5.6]);
     }
 }
